900e237b75a1e91426c6e7ae202359af8e2d3b81
Himanshu Rawat <himanshurawat0075407@gmail.com>
2024-01-29 00:11:01 +0530

    now we can display the date on out page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -45,45 +45,9 @@ cart.forEach(cartelement => {
     <div class="delivery-options-title">
       Choose a delivery option:
     </div>
-    <div class="delivery-option">
-      <input type="radio" checked
-        class="delivery-option-input"
-        name="delivery-option-${totalproduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Tuesday, June 21
-        </div>
-        <div class="delivery-option-price">
-          FREE Shipping
-        </div>
-      </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${totalproduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Wednesday, June 15
-        </div>
-        <div class="delivery-option-price">
-          $4.99 - Shipping
-        </div>
-      </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${totalproduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Monday, June 13
-        </div>
-        <div class="delivery-option-price">
-          $9.99 - Shipping
-        </div>
-      </div>
-    </div>
+    
+    ${deliveryOptionsHTML(totalproduct)}
+    
   </div>
 </div>
 </div>`;
@@ -93,7 +57,7 @@ cart.forEach(cartelement => {
 });
 
 
-function deliveryOptionsHTML() {
+function deliveryOptionsHTML(totalproduct) {
   let html='';
   deliveryOptions.forEach(deliveryOption => {
     const today = dayjs();
@@ -115,8 +79,8 @@ function deliveryOptionsHTML() {
       </div>
     </div>
   `
-  return html;
-  })
+})
+return html;
 }
 document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
   link.addEventListener('click', () => {
